3731bcead1eb9116fe8f49a57fb2f6ec2ddb268c
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-08-26 01:11:26 +0530

    .

--- Comperion.py
+++ Comperion.py
@@ -1,5 +1,14 @@
-a  = 34
+a  = 66
 b = 65
- 
 print(a>b)#false
 print(a<b)#true
+#find a is greater than b or not
+a  = int(input("enter the first number ")) 
+b  = int(input("enter the second number ")) 
+print("a is greter than b " , a>b )
+
+ #find average number of 2 number
+a  = int(input("enter the first number ")) 
+b  = int(input("enter the second number ")) 
+print("Average " , (a+b)/2 )
+
