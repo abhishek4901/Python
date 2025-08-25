9df67781fc408cec75920b75662b6a149d2820e5
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-08-26 01:01:03 +0530

    .

---
+++ Input.py
@@ -0,0 +1,9 @@
+a = input ("enter the first number " )
+b = input ("enter the second number " )
+print(a+b)# 1+1 = 11 because input take  like string then typcast in to int
+
+a = int(input ("enter the first number " ))#here string type cast in to int
+b = int(input ("enter the second number " ))
+print( "sum of number =" , a+b)#1+1 = 2
+print( "remainder = ", a%b)# 5%2 = 1 remiander
+
