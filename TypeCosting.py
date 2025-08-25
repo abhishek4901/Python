86d171500f4d10877e91b8feb10c0519a28d7208
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-08-26 00:46:19 +0530

    ,.

--- TypeCosting.py
+++ TypeCosting.py
@@ -8,3 +8,18 @@ print(d) # float
 a = "hello"
 B= type(a)
 print(B) #string
+
+ #type casting 
+
+x = "10.5" # String to float
+print(x)#string
+y = float(x)
+print(y)#flaot
+ 
+n = 5 # int to float
+m = float(n)
+print(m)# float 5.0
+
+p = 5.5# float to int
+m = int(p)
+print(m)# int 5
