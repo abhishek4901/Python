55e1d22ea703141bf582f442fa6276aaee4b8844
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-08-26 00:16:25 +0530

    .

---
+++ Externalmodule.py
@@ -0,0 +1,19 @@
+import pyttsx3
+engine = pyttsx3.init()
+
+# For Mac, If you face error related to "pyobjc" when running the `init()` method :
+# Install 9.0.1 version of pyobjc : "pip inpyobjstall c>=9.0.1"
+
+engine.say("""Twinkle, twinkle, little star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle, twinkle, little star
+How I wonder what you are
+Twinkle, twinkle, little star
+How I wonder what you are
+Up above the world so high   
+Like a diamond in the sky
+Twinkle, twinkle, little star
+How I wonder what you are""")
+engine.runAndWait()
---
+++ Variable.py
@@ -0,0 +1,16 @@
+a  = 10  #automatically assign value declear them
+b = 2.5
+c = "hello"
+d = False
+e = None
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+f = 4>5 #comparison operator return value boolean true or false 
+print(f)#false
+g = 4<5
+print(g)#true
+print(not(True))# output false , not operator
+
