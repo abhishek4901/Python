48da3e193a6d04e79db5c730d959808e9c0bebce
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-08-27 22:52:35 +0530

    Update Externalmodule.py

--- Basic/Externalmodule.py
+++ Basic/Externalmodule.py
@@ -6,7 +6,7 @@ engine = pyttsx3.init()
 
 engine.say("""Twinkle, twinkle, little star 
 How I wonder what you are
-Up above the world so high
+Up above the world so high 
 Like a diamond in the sky
 Twinkle, twinkle, little star
 How I wonder what you are
