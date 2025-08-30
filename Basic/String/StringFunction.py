e320451300121b70ca2f98ec1a719eda5a12e1e9
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-08-31 01:13:53 +0530

    .

---
+++ Basic/String/StringFunction.py
@@ -0,0 +1,6 @@
+d = "abhishek"
+print(len(d))
+print(d.startswith("ab"))#true
+print(d.startswith("wkqd")) #false
+print(d.endswith("wkqd")) #false
+print(d.capitalize()) #first word of letter is capital
