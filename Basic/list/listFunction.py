9bf2748ee4905cef25c18d29770efaf49225f1a7
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-08-31 15:01:59 +0530

    .

---
+++ Basic/list/listFunction.py
@@ -0,0 +1,27 @@
+a = [ 1,2,7,5,3,12,34,21]
+print(a)#[1, 2, 7, 5, 3, 12, 34, 21]
+ 
+#sort
+a.sort()
+print(a.sort())#none so not use this type
+print(a)#[1, 2, 3, 5, 7, 12, 21, 34]
+
+#append = insert at the end 
+print(a.append("Mahadev"))#non but next print add mahadev so not use this type
+a.append("ram")
+print(a)# [1, 2, 3, 5, 7, 12, 21, 34, 'Mahadev', 'ram']
+ 
+#insert add value any place
+a.insert(2, 3333333)
+print(a)#[1, 2, 3333333, 3, 5, 7, 12, 21, 34, 'Mahadev', 'ram']
+
+#remove()  remove only sigle value from list directly
+a.remove(12)#12 remove
+print(a)
+#a.remove("ram" , 1) #error
+a.remove("ram")
+print(a)#[1, 2, 3333333, 3, 5, 7, 21, 34, 'Mahadev']
+
+#pop() use to delete vale using index
+a.pop(1)
+print(a)#[1, 3333333, 3, 5, 7, 21, 34, 'Mahadev']
---
+++ Basic/list/listFunction1.py
@@ -0,0 +1,8 @@
+a = [ 1,2,7,5,3,12,34,21]
+print(a)#[1, 2, 7, 5, 3, 12, 34, 21]
+ 
+#reverse list
+#print(a.reverse())#none  only this line run  if run whole code then not working reverse function at down side also  this problem in only reverse
+a.reverse() #print statement ke andr nhi hoga kyoki ye interpreter se compile krta h  so  bahr hi rkhna hoga 
+
+print(a)#[21, 34, 12, 3, 5, 7, 2, 1]
