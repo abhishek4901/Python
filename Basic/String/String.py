cce4e4860e834acda44277201f8eba722a5f9b7c
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-08-31 10:55:42 +0530

    .

--- Basic/String/String.py
+++ Basic/String/String.py
@@ -4,5 +4,9 @@ b= a[0:2]#starting index : end index
 print(b)#he
 c = a[:3]#string index is zero
 print(c)#hel
+d = "i am good \"boy\" "
+print(d)
+e = 'i am good \'boy\' '
+print(e)
  
    
--- Basic/String/StringFunction.py
+++ Basic/String/StringFunction.py
@@ -4,3 +4,4 @@ print(d.startswith("ab"))#true
 print(d.startswith("wkqd")) #false
 print(d.endswith("wkqd")) #false
 print(d.capitalize()) #first word of letter is capital
+print(d.replace("abhi", "nhi"))#nhishek
