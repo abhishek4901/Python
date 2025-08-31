b8bfe636787c61bb129ae8199647d1a72c15016d
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-08-31 11:15:12 +0530

    .

--- Basic/Input.py
+++ Basic/Input.py
@@ -1,9 +1,12 @@
-a = input ("enter the first number " )
-b = input ("enter the second number " )
-print(a+b)# 1+1 = 11 because input take  like string then typcast in to int
+# a = input ("enter the first number " )
+# b = input ("enter the second number " )
+# print(a+b)# 1+1 = 11 because input take  like string then typcast in to int
 
-a = int(input ("enter the first number " ))#here string type cast in to int
-b = int(input ("enter the second number " ))
-print( "sum of number =" , a+b)#1+1 = 2
-print( "remainder = ", a%b)# 5%2 = 1 remiander
+# a = int(input ("enter the first number " ))#here string type cast in to int
+# b = int(input ("enter the second number " ))
+# print( "sum of number =" , a+b)#1+1 = 2
+# print( "remainder = ", a%b)# 5%2 = 1 remiander
 
+a = input("enter the name ")
+print(f"Good after noon {a}") #input show dishplay
+print("Good after noon " + a )#this is also work
