9f527c5cbefb66932ae15947b16adc71afae4fb8
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-08-31 11:04:38 +0530

    .

--- Basic/String/StringFunction.py
+++ Basic/String/StringFunction.py
@@ -1,7 +1,15 @@
 d = "abhishek"
 print(len(d))
+#start function 
 print(d.startswith("ab"))#true
 print(d.startswith("wkqd")) #false
+#end function
 print(d.endswith("wkqd")) #false
+#capitale
 print(d.capitalize()) #first word of letter is capital
+#replace
 print(d.replace("abhi", "nhi"))#nhishek
+#find
+b = "hello python "
+c= b.find("lo")
+print(c)#output 3 because index 3 has start Lo "jaha se index srart hoga find value ka vo index return krta  h "
