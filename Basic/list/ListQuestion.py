fa023576e48ba90ef2274785d7df0e0a170c0e5f
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-09-27 10:47:40 +0530

    .

---
+++ Basic/list/ListQuestion.py
@@ -0,0 +1,29 @@
+import collections 
+
+#Create the random 10  number  of the list
+list1 = []
+for i in range(4):
+    num = int(input("Enter number :"  ))
+    list1.append(num)
+print(list1)
+
+#display the list the maximun number of list
+n = max(list1)
+print(" maximum number in the list", n )
+
+#display the list the smallest number of list
+n = min(list1)
+print(" minimum number in the list", n )
+
+#sort item from the list 
+n = sorted(list1)
+print("sorted list :" ,n)
+
+
+# display frequency of each number
+frequency = collections.Counter(list1)
+print("frequency :", frequency )
+
+#remove the  dublicate 
+n = list(set(list1))
+print("remove the dublicates then list",n)
