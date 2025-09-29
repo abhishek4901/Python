47843efdbb184696175a07b59891b1f0a30fe161
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-09-29 17:50:50 +0530

    Update Externalmodule.py

--- Basic/Externalmodule.py
+++ Basic/Externalmodule.py
@@ -9,7 +9,7 @@ How I wonder what you are
 Up above the world so high  
 Like a diamond in the sky
 Twinkle, twinkle, little star
-How I wonder what you are
+How I wonder what you are 
 Twinkle, twinkle, little star
 How I wonder what you are
 Up above the world so high   
