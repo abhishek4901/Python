77f41ae9d35eb180b754eb7350369d6a6f0633b2
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-10-01 19:34:52 +0530

    Update String.py

--- Basic/String/String.py
+++ Basic/String/String.py
@@ -3,7 +3,7 @@ print(a)#hello
 b= a[0:2]#starting index : end index 
 print(b)#he
 c = a[:3]#string index is zero
-print(c)#hel
+print(c)#hel 
 #escape sequnce charecter  
 d = "i am good \"boy\" "#there print \boy\
 print(d)
