b217b93dbcc5ac29ca2d8455af2c8b5228b46556
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-10-02 23:45:51 +0530

    .

---
+++ Basic/FileQestion/Countfreq6.py
@@ -0,0 +1,17 @@
+# Take input string
+text = input("Enter a string: ")
+
+# Dictionary to store frequency
+freq = {}
+
+# Count frequency of each character
+for char in text:
+    if char in freq:
+        freq[char] += 1
+    else:
+        freq[char] = 1
+
+# Display result
+print("Character Frequency:")
+for key, value in freq.items():
+    print(key, ":", value)
