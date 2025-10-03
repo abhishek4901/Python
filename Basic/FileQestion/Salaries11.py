94651e4983127e270eabe1f5dea7c9669a3e1d81
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-10-03 12:09:16 +0530

    .

---
+++ Basic/FileQestion/Salaries11.py
@@ -0,0 +1,12 @@
+# Take input from user
+salaries = list(map(float, input("Enter employee salaries separated by space: ").split()))
+
+# New list to store updated salaries
+updated_salaries = []
+
+for sal in salaries:
+    new_sal = sal + (sal * 0.10)   # increase salary by 10%
+    updated_salaries.append(new_sal)
+
+print("Original Salaries:", salaries)
+print("Updated Salaries:", updated_salaries)
