a7427363ac3d475bb4ef63c76e23a339912105b7
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-10-05 23:42:26 +0530

    .

---
+++ Exam Question/Palidrome.py
@@ -0,0 +1,25 @@
+    #slicing()
+   
+s = input("enter the string ")
+s_clean = s.lower()
+
+if s_clean[::-1]:
+    print(f"'{s}' is a palindrome")
+else:
+    print(f"'{s}' is not a palindrome")
+    
+#Method 2: Using a loop
+s = input("enter the string ")
+s_clean = s.lower()
+is_palindrome = True
+
+for i in range(len(s_clean)//2):
+    if s_clean[i] != s_clean[-(i+1)]:
+        is_palindrome = False
+        break
+
+if is_palindrome:
+    print(f"'{s}' is a palindrome")
+else:
+    print(f"'{s}' is not a palindrome")
+
---
+++ Exam Question/ReverseString.py
@@ -0,0 +1,32 @@
+#1️⃣ Using Indexing
+
+#You can loop through the string from the end to the start using negative indexing:
+
+s = input("enter the sstring ")
+reversed_s = ""
+
+# Loop from last index to first
+for i in range(len(s)-1, -1, -1):
+    reversed_s += s[i]
+
+print(reversed_s)  # Output: nohtyP
+
+
+#2️ Using Slicing
+
+s = input("enter the  String ")
+reversed_s = s[::-1]
+print(reversed_s)  # Output: nohtyP
+
+
+#recursion
+def reverse_string(s):
+    # Base case: if string is empty or single char
+    if len(s) <= 1:
+        return s
+    # Recursive case: last char + reverse of rest
+    return s[-1] + reverse_string(s[:-1])
+
+s =input("enter the String ")
+reversed_s = reverse_string(s)
+print(reversed_s)  # Output: nohtyP
