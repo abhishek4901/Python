2b824283ee7a5ef6fcfd5edd756fa9de5969d9ed
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-10-05 19:51:21 +0530

    Update hi.py

--- Exam Question/hi.py
+++ Exam Question/hi.py
@@ -1,6 +1,18 @@
-import sys
-a =10
-# print(sys.getsize(int))
-print(type([]))#class type list
-print(id(int))# address of a
-print(sys.getsizeof(a))#size of a 24int +4
+# import sys
+# a =10
+# # print(sys.getsize(int))
+# print(type([]))#class type list
+# print(id(int))# address of a
+# print(sys.getsizeof(a))#size of a 24int +4
+s = "Hello"
+# s[0] = "h"  ❌ Error, cannot change string characters
+
+# Correct way to "change" string:
+# s2 = "h" + s[3:]  # Create a new string
+# print(s2)         # hello
+
+numbers = [1, 2, 3,4,5,7]
+# numbers.append(4)        # Add 4 at the end → [1,2,3,4]
+# numbers.insert(1, 5)     # Insert 5 at index 1 → [1,5,2,3,4]
+numbers.remove(7)        # Remove 2 → [1,5,3,4]
+print(numbers)
