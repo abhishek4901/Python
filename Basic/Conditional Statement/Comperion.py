dff23f3345022c1cfa2d228836ecb66b8e49819b
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-10-06 19:41:50 +0530

    Update Comperion.py

--- Basic/Conditional Statement/Comperion.py
+++ Basic/Conditional Statement/Comperion.py
@@ -7,7 +7,7 @@ a  = int(input("enter the first number "))
 b  = int(input("enter the second number ")) 
 print("a is greter than b " , a>b )
 
- #find average number of 2 number
+ #find average number of 2 number 
 a  = int(input("enter the first number ")) 
 b  = int(input("enter the second number ")) 
 print("Average " , (a+b)/2 )
