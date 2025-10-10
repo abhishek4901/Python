4c8a9f3b14c34973b3c70dd167f2eaa3db6e651c
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-10-10 22:55:16 +0530

    Update 11.SalariesInrement.py

--- FileQestion/11.SalariesInrement.py
+++ FileQestion/11.SalariesInrement.py
@@ -6,7 +6,7 @@ updated_salaries = []
 
 for sal in salaries:
     new_sal = sal + (sal * 0.10)   # increase salary by 10%
-    updated_salaries.append(new_sal) 
+    updated_salaries.append(new_sal)  
 
 print("Original Salaries:", salaries)
 print("Updated Salaries:", updated_salaries)
