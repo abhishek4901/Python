b407c396b49762db89a7eea3d315614bfcdeab40
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-10-12 18:55:27 +0530

    Update Anagram.py

--- Exam Question/Anagram.py
+++ Exam Question/Anagram.py
@@ -7,7 +7,7 @@ str2 = s2.lower().replace(" ", "")
 
 # Check if sorted strings are equal  
 if sorted(str1) == sorted(str2):   
-    print(f"'{s1}' and '{s2}' are anagrams")
+    print(f"'{s1}' and '{s2}' are anagrams") 
 else: 
     print(f"'{s1}' and '{s2}' are not anagrams") 
 
