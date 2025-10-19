ffbe222233f5b5f4271d9673f7aa1beb327e10f9
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-10-19 21:22:53 +0530

    Clarify character frequency counting with comment
    Added a comment to clarify the purpose of the code.

--- FileQestion/6.Countfreq.py
+++ FileQestion/6.Countfreq.py
@@ -4,7 +4,7 @@ text = input("Enter a string: ")
 # Dictionary to store frequency
 freq = {}
 
-# Count frequency of each character
+# Count frequency of each character 
 for char in text:
     if char in freq:
         freq[char] += 1 
