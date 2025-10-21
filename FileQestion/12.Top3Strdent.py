7bd4d8096ad2c329079cc3140f390be14c369709
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-10-21 22:16:08 +0530

    Update 12.Top3Strdent.py

--- FileQestion/12.Top3Strdent.py
+++ FileQestion/12.Top3Strdent.py
@@ -4,7 +4,7 @@ scores = list(map(int, input("Enter student scores separated by space: ").split(
 # Sort the list in descending order
 for i in range(len(scores)):
     for j in range(i + 1, len(scores)):
-        if scores[i] < scores[j]: 
+        if scores[i] < scores[j]:  
             # swap values
             scores[i], scores[j] = scores[j], scores[i]
 
