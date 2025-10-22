88a244e19f24c99e871f74c9d0ee9c63753b791b
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-10-22 23:36:43 +0530

    Fix formatting issue in rotate list code

--- Exam Question/RototeListKPostion.py
+++ Exam Question/RototeListKPostion.py
@@ -4,7 +4,7 @@ k = int(input("Enter the position to rotate: "))
 
 # Left rotate  
 k = k % len(nums)  # in case k > length 
-rotated = nums[k:] + nums[:k]
+rotated = nums[k:] + nums[:k] 
  
 print("Rotated list:", rotated)
  
