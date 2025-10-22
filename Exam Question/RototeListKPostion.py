f693ccf734b9927c625973276a586949b790ea88
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-10-22 23:35:32 +0530

    Fix comment formatting in rotation script

--- Exam Question/RototeListKPostion.py
+++ Exam Question/RototeListKPostion.py
@@ -2,7 +2,7 @@
 nums = list(map(int, input("Enter numbers separated by space: ").split()))
 k = int(input("Enter the position to rotate: "))
 
-# Left rotate
+# Left rotate 
 k = k % len(nums)  # in case k > length
 rotated = nums[k:] + nums[:k]
  
