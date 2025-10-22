0ac5500290961b0a6ecf8e036f4c0fe98b33536d
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-10-22 23:35:47 +0530

    Update RototeListKPostion.py

--- Exam Question/RototeListKPostion.py
+++ Exam Question/RototeListKPostion.py
@@ -3,7 +3,7 @@ nums = list(map(int, input("Enter numbers separated by space: ").split()))
 k = int(input("Enter the position to rotate: "))
 
 # Left rotate  
-k = k % len(nums)  # in case k > length
+k = k % len(nums)  # in case k > length 
 rotated = nums[k:] + nums[:k]
  
 print("Rotated list:", rotated)
