528f780befd211481a24db0d35a157db2aa68f27
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-10-24 20:20:25 +0530

    Update 3.StopWord.py

--- FileQestion/3.StopWord.py
+++ FileQestion/3.StopWord.py
@@ -5,7 +5,7 @@ stop_words = ["is", "the", "a", "an", "of", "in", "on", "and", "to", "for"]
 text = input("Enter a sentence: ")
 
 # Remove stop words using a simple loop
-result = ""
+result = "" 
 for word in text.split():
     if word.lower() not in stop_words:
         result += word + " "
