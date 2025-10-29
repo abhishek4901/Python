8f71dd6a3f3c97dd2dde1e078cd624642d4af1c9
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-10-29 06:34:05 +0530

    Update Prime.py

--- Exam Question/Prime.py
+++ Exam Question/Prime.py
@@ -6,7 +6,7 @@ if num > 1:
     for i in range(2, int(num ** 0.5) + 1):
         if num % i == 0:
             print(num, "is not a prime number.")
-            break
+            break 
     else:
         print(num, "is a prime number.")
 else: 
