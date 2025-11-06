38f48ef216994260a8ea570a8413a49a07389af2
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-11-07 02:13:52 +0530

    Fix formatting of match case for vowels

--- Basic/Switch/CheckVowel.py
+++ Basic/Switch/CheckVowel.py
@@ -2,7 +2,7 @@ n = input("Enter a character: ")
 ch = n[0]
  
 match ch: #block banne liye :
-    case 'a'|'A'|'e'|'E'|'i'|'I'|'o'|'O'|'u'|'U' :
+    case 'a'|'A'|'e'|'E'|'i'|'I'|'o'|'O'|'u'|'U' : 
         print("vowel")
     case _ :#default case like this
         print("Consonent")
